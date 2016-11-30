03ee4a971eee2fe6ed33194f73b435bf3143bdc9
ice <smalltides@gmail.com>
2016-12-01 04:03:16 +0800

    add ListView Demo

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,8 @@ import {
   AppRegistry,
   View,
   Text,
-  ScrollView
+  ScrollView,
+  Image
 } from 'react-native';
 
 import {
@@ -17,17 +18,30 @@ import {
   ActionSheet,
   Card,
   Drawer,
-  List
+  List,
+  ListView
 } from 'antd-mobile';
 
 export default class antd extends Component {
   state = {
     selectedTab: 'A',
-    actionSheet: ''
+    actionSheet: '',
+    listViewDataSource: null,
+    listViewIsLoading: false
   };
 
+  componentWillMount() {
+    this.initListViewDataSource();
+  }
+
+
   render() {
-    return this.useList();
+
+
+    //Very bad style
+    return this.useListView();
+
+    //return this.useList();
     //return this.useDrawer();
 
     //return this.useCard();
@@ -37,16 +51,98 @@ export default class antd extends Component {
     //return this.useNoticeBar();
   }
 
-  useList() {
-    const Item = List.Item;
-    const Brief = Item.Brief;
+  useListView() {
+     const data = [
+      {
+        img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
+        title: '相约酒店',
+        des: '不是所有的兼职汪都需要风吹日晒',
+      },
+      {
+        img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
+        title: '麦当劳邀您过周末',
+        des: '不是所有的兼职汪都需要风吹日晒',
+      },
+      {
+        img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
+        title: '食惠周',
+        des: '不是所有的兼职汪都需要风吹日晒',
+      },
+    ];
+    let index = data.length - 1;
+
+    const row = (rowData, sectionID, rowID) => {
+      if (index < 0) {
+        index = data.length - 1;
+      }
+      const obj = data[index--];
+
+      return (
+        <View key={rowID}>
+          <Text style={{ padding: 2, marginBottom: 8 }}>
+            {obj.title}
+          </Text>
+
+          <View>
+            <Image style={{ height: 64, width: 64, marginRight: 8 }} source={{ uri: obj.img }} />
+            <View>
+              <Text>{obj.des}</Text>
+              <Text>{rowID}元/任务</Text>
+            </View>
+          </View>
+
+        </View>
+      );
+    };
 
     return (
-      <ScrollView>
+      <ListView
+        dataSource={this.state.listViewDataSource}
+        renderHeader={() => <Text>Header</Text>}
+        renderFooter={() => <View>{this.state.listViewIsLoading ? <Text>加载中...</Text> : <Text>加载完毕</Text>}</View>}
+        renderRow={row}
+        pageSize={4}
+        scrollRenderAheadDistance={500}
+        scrollEventThrottle={20}
+        onScroll={() => { console.log('scroll'); }}
+        useBodyScroll
+        onEndReachedThreshold={10}
+        />
+    );
+  }
+
+  initListViewDataSource() {
+    const NUM_ROWS = 20;
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+
+    this.genData = (pIndex = 0) => {
+      const dataBlob = {};
+      for (let i = 0; i < NUM_ROWS; i++) {
+        const ii = (pIndex * NUM_ROWS) + i;
+        dataBlob[`${ii}`] = `row - ${ii}`;
+      }
+      return dataBlob;
+    };
+    this.rData = this.genData();
+
+    this.setState({
+      listViewDataSource: dataSource.cloneWithRows(this.rData),
+      listViewIsLoading: false
+    });
+}
+
+useList() {
+  const Item = List.Item;
+  const Brief = Item.Brief;
+
+  return (
+    <ScrollView>
       <List
         renderHeader={() => '我是华丽丽的列表头部'}
         renderFooter={() => '我是列表尾部'}
-      >
+        >
         <Item style={{ flex: 0 }} thumb="https://cloud.githubusercontent.com/assets/1698185/18039916/f025c090-6dd9-11e6-9d86-a4d48a1bf049.png" data-seed="logId">标题文字点击无反馈，文字超长则隐藏</Item>
         <Item style={{ flex: 0 }} wrap>文字超长折行文字超长折行文字超长折行文字超长折行文字超长折行文字超长折行</Item>
         <Item style={{ flex: 0 }} onClick={() => Toast.info('点击触发onClick事件')}>内容内容 Click Me</Item>
@@ -76,168 +172,168 @@ export default class antd extends Component {
         <Item style={{ flex: 0 }} extra="没有箭头" arrow="empty">标题文字</Item>
 
       </List>
-      </ScrollView>
-    );
-  }
+    </ScrollView>
+  );
+}
 
-  useDrawer() {
-    return (
-      <Drawer
-        sidebar={<Button onClick={() => this.refs.drawer.drawer.closeDrawer()}>Close Sidebar</Button>}
-        position='left'
-        ref='drawer'
+useDrawer() {
+  return (
+    <Drawer
+      sidebar={<Button onClick={() => this.refs.drawer.drawer.closeDrawer()}>Close Sidebar</Button>}
+      position='left'
+      ref='drawer'
       >
       <Button onClick={() => this.refs.drawer.drawer.openDrawer()}>Open Drawer</Button>
-      </Drawer>
-    );
-  }
+    </Drawer>
+  );
+}
 
-  useCard() {
-    return (
-      <WingBlank size="lg">
-        <WhiteSpace size="lg" />
-        <Card>
-          <Card.Header
-            title="这是 title"
-            thumb="https://cloud.githubusercontent.com/assets/1698185/18039916/f025c090-6dd9-11e6-9d86-a4d48a1bf049.png"
-            thumbStyle={{ width: 30, height: 30 }}
-            extra={'this is extra'}
+useCard() {
+  return (
+    <WingBlank size="lg">
+      <WhiteSpace size="lg" />
+      <Card>
+        <Card.Header
+          title="这是 title"
+          thumb="https://cloud.githubusercontent.com/assets/1698185/18039916/f025c090-6dd9-11e6-9d86-a4d48a1bf049.png"
+          thumbStyle={{ width: 30, height: 30 }}
+          extra={'this is extra'}
           />
-          <Card.Body>
-            <Text>这是卡片内容</Text>
-          </Card.Body>
-          <Card.Footer content={'这是卡尾'} extra={'这是尾部介绍'} />
-        </Card>
-        <WhiteSpace size="lg" />
-      </WingBlank>
-    );
-  }
+        <Card.Body>
+          <Text>这是卡片内容</Text>
+        </Card.Body>
+        <Card.Footer content={'这是卡尾'} extra={'这是尾部介绍'} />
+      </Card>
+      <WhiteSpace size="lg" />
+    </WingBlank>
+  );
+}
 
-  useActionSheet() {
-    const BUTTONS = ['操作一', '操作二', '操作三', '删除', '取消'];
-    return (
-      <Button
-        onClick={
-          () => {
-            ActionSheet.showActionSheetWithOptions({
-              options: BUTTONS,
-              cancelButtonIndex: BUTTONS.length - 1,
-              destructiveButtonIndex: BUTTONS.length - 2,
-              // title: '标题',
-              message: '我是描述我是描述',
-              maskClosable: true,
-              'data-seed': 'logId',
-            },
-              (buttonIndex) => {
-                this.setState({ actionSheet: BUTTONS[buttonIndex] });
-              });
-          }
+useActionSheet() {
+  const BUTTONS = ['操作一', '操作二', '操作三', '删除', '取消'];
+  return (
+    <Button
+      onClick={
+        () => {
+          ActionSheet.showActionSheetWithOptions({
+            options: BUTTONS,
+            cancelButtonIndex: BUTTONS.length - 1,
+            destructiveButtonIndex: BUTTONS.length - 2,
+            // title: '标题',
+            message: '我是描述我是描述',
+            maskClosable: true,
+            'data-seed': 'logId',
+          },
+            (buttonIndex) => {
+              this.setState({ actionSheet: BUTTONS[buttonIndex] });
+            });
         }
+      }
       >
-        ShowActionSheet
+      ShowActionSheet
        </Button>
-    );
-  }
+  );
+}
 
-  useTabBar() {
-    return (
-      <TabBar
-        unselectedTintColor="#949494"
-        tintColor="#33A3F4"
-        barTintColor="white"
+useTabBar() {
+  return (
+    <TabBar
+      unselectedTintColor="#949494"
+      tintColor="#33A3F4"
+      barTintColor="white"
       >
-        <TabBar.Item
-          title="A"
-          key="A"
-          selected={this.state.selectedTab === 'A'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'A',
-            });
-          }}
+      <TabBar.Item
+        title="A"
+        key="A"
+        selected={this.state.selectedTab === 'A'}
+        onPress={() => {
+          this.setState({
+            selectedTab: 'A',
+          });
+        } }
         >
-          <Button>A</Button>
-        </TabBar.Item>
-        <TabBar.Item
-          title="B"
-          key="B"
-          badge={3}
-          selected={this.state.selectedTab === 'B'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'B',
-            });
-          }}
+        <Button>A</Button>
+      </TabBar.Item>
+      <TabBar.Item
+        title="B"
+        key="B"
+        badge={3}
+        selected={this.state.selectedTab === 'B'}
+        onPress={() => {
+          this.setState({
+            selectedTab: 'B',
+          });
+        } }
         >
-          <Button>B</Button>
-        </TabBar.Item>
-        <TabBar.Item
-          title="C"
-          key="C"
-          selected={this.state.selectedTab === 'C'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'C',
-            });
-          }}
+        <Button>B</Button>
+      </TabBar.Item>
+      <TabBar.Item
+        title="C"
+        key="C"
+        selected={this.state.selectedTab === 'C'}
+        onPress={() => {
+          this.setState({
+            selectedTab: 'C',
+          });
+        } }
         >
-          <Button>C</Button>
-        </TabBar.Item>
-        <TabBar.Item
-          title="D"
-          key="D"
-          selected={this.state.selectedTab === 'D'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'D',
-            });
-          }}
+        <Button>C</Button>
+      </TabBar.Item>
+      <TabBar.Item
+        title="D"
+        key="D"
+        selected={this.state.selectedTab === 'D'}
+        onPress={() => {
+          this.setState({
+            selectedTab: 'D',
+          });
+        } }
         >
-          <Button>D</Button>
-        </TabBar.Item>
-      </TabBar>
-    );
-  }
+        <Button>D</Button>
+      </TabBar.Item>
+    </TabBar>
+  );
+}
 
-  useSearchBar() {
-    return (
-      <SearchBar placeholder="搜索" />
-    );
-  }
+useSearchBar() {
+  return (
+    <SearchBar placeholder="搜索" />
+  );
+}
 
-  useNoticeBar() {
-    return (
-      <View>
-        <WhiteSpace size="lg" />
-        <NoticeBar mode="closable" onClick={this.onClick}>
-          国庆期间余额宝收益和转出到账时间1
+useNoticeBar() {
+  return (
+    <View>
+      <WhiteSpace size="lg" />
+      <NoticeBar mode="closable" onClick={this.onClick}>
+        国庆期间余额宝收益和转出到账时间1
         </NoticeBar>
-        <WhiteSpace size="lg" />
-        <NoticeBar type="info" mode="closable">国庆期间余额宝收益和转出到账时间</NoticeBar>
-        <WhiteSpace size="lg" />
+      <WhiteSpace size="lg" />
+      <NoticeBar type="info" mode="closable">国庆期间余额宝收益和转出到账时间</NoticeBar>
+      <WhiteSpace size="lg" />
 
-        <WhiteSpace size="lg" />
-        <NoticeBar mode="link" onClick={() => Toast.show('click')} data-seed="logId">
-          国庆期间余额宝收益和转出到账时间
+      <WhiteSpace size="lg" />
+      <NoticeBar mode="link" onClick={() => Toast.show('click')} data-seed="logId">
+        国庆期间余额宝收益和转出到账时间
         </NoticeBar>
-        <WhiteSpace size="lg" />
-        <NoticeBar mode="link" type="info">国庆期间余额宝收益和转出到账时间</NoticeBar>
-        <WhiteSpace size="lg" />
+      <WhiteSpace size="lg" />
+      <NoticeBar mode="link" type="info">国庆期间余额宝收益和转出到账时间</NoticeBar>
+      <WhiteSpace size="lg" />
 
-        <WhiteSpace size="lg" />
-        <NoticeBar type="info" onClick={() => Toast.show('click')}>国庆期间余额宝收益和转出到账时间</NoticeBar>
-        <WhiteSpace size="lg" />
-        <NoticeBar type="warn">国庆期间余额宝收益和转出到账时间</NoticeBar>
-        <WhiteSpace size="lg" />
-        <NoticeBar mode="closable" type="success">国庆期间余额宝收益和转出到账时间</NoticeBar>
-        <WhiteSpace size="lg" />
-        <NoticeBar type="error">国庆期间余额宝收益和转出到账时间</NoticeBar>
-        <WhiteSpace size="lg" />
-        <NoticeBar mode="link" type="question">国庆期间余额宝收益和转出到账时间</NoticeBar>
-        <WhiteSpace size="lg" />
-      </View>
-    );
-  }
+      <WhiteSpace size="lg" />
+      <NoticeBar type="info" onClick={() => Toast.show('click')}>国庆期间余额宝收益和转出到账时间</NoticeBar>
+      <WhiteSpace size="lg" />
+      <NoticeBar type="warn">国庆期间余额宝收益和转出到账时间</NoticeBar>
+      <WhiteSpace size="lg" />
+      <NoticeBar mode="closable" type="success">国庆期间余额宝收益和转出到账时间</NoticeBar>
+      <WhiteSpace size="lg" />
+      <NoticeBar type="error">国庆期间余额宝收益和转出到账时间</NoticeBar>
+      <WhiteSpace size="lg" />
+      <NoticeBar mode="link" type="question">国庆期间余额宝收益和转出到账时间</NoticeBar>
+      <WhiteSpace size="lg" />
+    </View>
+  );
+}
 
 }
 
