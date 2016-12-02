91cf45f6e7e5d0aaf80d6469d12557f126c07d1f
ice <smalltides@gmail.com>
2016-12-02 22:24:59 +0800

    add Checkbox Demo

--- index.ios.js
+++ index.ios.js
@@ -27,7 +27,8 @@ import {
   Popup,
   ActivityIndicator,
   Badge,
-  Carousel
+  Carousel,
+  Checkbox
 } from 'antd-mobile';
 
 export default class antd extends Component {
@@ -47,7 +48,9 @@ export default class antd extends Component {
 
   render() {
 
-    return this.useCarousel();
+    return this.useCheckbox();
+
+    //return this.useCarousel();
 
     //return this.useBadge();
     //return this.useButton();
@@ -68,6 +71,60 @@ export default class antd extends Component {
     //return this.useTabBar();
     //return this.useSearchBar();
     //return this.useNoticeBar();
+  }
+
+  useCheckbox() {
+    const AgreeItem = Checkbox.AgreeItem;
+    const CheckboxItem = Checkbox.CheckboxItem;
+
+    return (
+       <View>
+
+        <View style={{ padding: 10 }}>
+          <Checkbox checked={this.state.checkBox1} style={{tintColor:'#f00'}} onChange={(event) => {
+            this.setState({
+              checkBox1: event.target.checked,
+            });
+          }} />
+          <WhiteSpace />
+          <Checkbox>Checkbox</Checkbox>
+          <WhiteSpace />
+          <Checkbox checked disabled />
+          <WhiteSpace />
+          <Checkbox disabled />
+        </View>
+        <WhiteSpace />
+
+        <AgreeItem>同意同意同意同意同意同意同意同意同意同意同意同意同意同意同意同意同意同意同意同意同意</AgreeItem>
+        <WhiteSpace />
+        <AgreeItem checked={this.state.agreeItem1} checkboxStyle={{tintColor:'#f00'}} onChange={(event) => {
+            this.setState({
+              agreeItem1: event.target.checked,
+            });
+          }}
+        >同意 《信用支付服务合同》
+        </AgreeItem>
+
+        <WhiteSpace />
+        <AgreeItem disabled>未选中，不可编辑 《信用支付服务合同》</AgreeItem>
+        <WhiteSpace />
+        <AgreeItem checked disabled>强制选中,不可编辑 《信用支付服务合同信用支付服务合同信用支付服务合同》</AgreeItem>
+
+        <List style={{marginTop: 30,}}>
+          <Text style={{marginTop: 12}}>表单多选项，普通列表中多选项</Text>
+          <CheckboxItem style={{flex: 0}} checked={this.state.checkboxItem1} onChange={(event) => {
+            this.setState({
+              checkboxItem1: event.target.checked,
+            });
+          }}
+          >签约
+          </CheckboxItem>
+          <CheckboxItem style={{flex: 0}}>物料铺设</CheckboxItem>
+          <CheckboxItem style={{flex: 0}} disabled>机具维护（不能选）</CheckboxItem>
+          <CheckboxItem style={{flex: 0}} disabled checked>产品问题解决（必选）</CheckboxItem>
+        </List>
+      </View>
+    );
   }
 
   useCarousel() {
